d5a39ba88f67cb3134a36a1a82187f9dd087ec13
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-05-14 09:36:16 -0300

    added initial functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = null;
+var lowercase = null;
+var uppercase = null;
+var numeric = null;
+var specialChar = null;
+
+  function generatePassword () {
+    getLength();
+    getCharTypes();
+}
+    if (lowercase){
+      selection += `abcdefghijklmnopqurtuvwxyz`;
+    }
+    if (uppercase){
+      `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    }
+    if (numeric){
+      selection += `1234567890`;
+    }
+    if (specialChar) {
+      selection += "!#$%&()*+,-./:;<=>?@[}^_`{|}~'";
+    }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
