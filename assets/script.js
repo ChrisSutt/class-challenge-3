3a6e8255f7c318de4fef9fb55e41ff46146919ef
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-05-14 09:58:59 -0300

    added first function for get length

--- assets/script.js
+++ assets/script.js
@@ -7,22 +7,54 @@ var uppercase = null;
 var numeric = null;
 var specialChar = null;
 
-  function generatePassword () {
-    getLength();
-    getCharTypes();
+function generatePassword(){
+  getLength();
+  getCharTypes();
+  
+  var selection = '';
+  var result = '';
+
+  if(lowercase){
+    selection += 'abcdefghijklmnopqrstuvwxyz';
+  }
+  if(uppercase){
+    selection += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  if(numeric){
+    selection += '1234567890';
+  }
+  
+  if(specialChar){
+    selection += '!#$%&()*+,-./:;<=>?@[}^_`{|}~';
+  }
+  
+  const selectionLength = selection.length;
+  for(x = 0; x < length; x++){
+    result += selection.charAt(Math.floor(Math.random() * selectionLength));
+  }
+
+  return result;
 }
-    if (lowercase){
-      selection += `abcdefghijklmnopqurtuvwxyz`;
+
+function getLength(){
+  length = prompt("Select Password Length", "At lease 8 characters, no more than 128 characters");
+  
+  if (length < 8 || length > 128){
+    alert("Password must be greater then 8 characters!")
+    if(length === null){
+      return "Your Secure Password";
     }
-    if (uppercase){
-      `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-    }
-    if (numeric){
-      selection += `1234567890`;
-    }
-    if (specialChar) {
-      selection += "!#$%&()*+,-./:;<=>?@[}^_`{|}~'";
-    }
+    getLength();
+  }
+  else if(isNaN(length)){
+    alert("A number between 8 and 128 must be selected!");
+    length = null;
+    getLength();
+  }
+  console.log("Character Count:" + length);
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
